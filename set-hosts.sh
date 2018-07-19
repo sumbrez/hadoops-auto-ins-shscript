bd623c959aa1e009bff02b54d1347fe2eccbbe81
sumbrez <xltzc@msn.cn>
2018-07-19 22:52:59 +0800

    修复set-hosts的bug（无法直接echo到hosts）

--- set-hosts.sh
+++ set-hosts.sh
@@ -7,12 +7,11 @@ if [ ! -e "/etc/hosts.backup" ]; then
     sudo cp /etc/hosts /etc/hosts.backup
 fi
 
+hname=`hostname`
 sudo cat hosts hosts-ext > hosts-all
+sudo echo "127.0.0.1 $hname" >> hosts-all
 sudo cp hosts-all /etc/hosts
 rm hosts-all
-
-hname=`hostname`
-sudo echo "127.0.0.1 $hname" >> /etc/hosts
 
 #sudo cp hosts /etc/hosts
 #sudo cat hosts-ext >> /etc/hosts
